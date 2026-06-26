a369bf0766849d5db8dd3033a2195547ccded7be
shihj <shihj@sina.cn>
2026-06-26 21:27:43 +0800

    Switch to HashRouter for GitHub Pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import '@/styles/index.css'
 import '@/styles/katex-overrides.css'
@@ -10,8 +10,8 @@ import '@/styles/animation.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
